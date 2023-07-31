6b4a24850232f0f34f69fd843164439ebfa4f925
Neil <nevinod@Neils-MacBook-Air.local>
2023-07-30 17:08:38 -0700

    add "Week" column to csv

--- Python/fantasy_weekly.py
+++ Python/fantasy_weekly.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
-weeks = range(1, 17)
-years = range(2016, 2022)
+WEEK_MIN = 1
+WEEK_MAX = 17
+
+YEAR_MIN = 2016
+YEAR_MAX = 2022
+
+weeks = range(WEEK_MIN, WEEK_MAX)
+years = range(YEAR_MIN, YEAR_MAX)
 
 def main():
     for year in years:
@@ -10,8 +16,10 @@ def main():
         for week in weeks:
             df = pd.read_csv(f'https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/{year}/week{week}.csv')
             df['Year'] = year
+            df['Week'] = week
+            
             yearly_df = pd.concat([yearly_df, df])
     
-        yearly_df.to_csv(f'Fantasy_{year}.csv')
+        yearly_df.to_csv(f'FF_Weekly_{year}.csv', index=False)
 
 main()
